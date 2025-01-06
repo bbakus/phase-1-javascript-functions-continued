80a5ce3f6ed2bc8c5506dddc2206d58776c73081
Brandon Bakus <brandonbakus@gmail.com>
2025-01-05 19:53:41 -0500

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // code your solution here
+
+
+function saturdayFun(activity){
+    if(activity == null){
+        activity = 'roller-skate'
+    }    
+    const saturday = `This Saturday, I want to ${activity}!`
+    
+    return saturday
+}
+
+function mondayWork(dull){
+    if(dull == null){
+       dull = 'go to the office'
+    }
+    const mondays = `This Monday, I will ${dull}.`
+    return mondays
+}
+
+function wrapAdjective(flair = '*'){
+    return function innerfunction(echo = "special"){
+        return `You are ${flair}${echo}${flair}!`
+    }
+
+}
